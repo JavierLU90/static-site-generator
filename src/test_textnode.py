a4c7db2781ca4357ef29c1915869eb5608c2173b
JavierLU90 <jlomelinu@gmail.com>
2025-03-24 14:29:59 -0600

    Added text-node-to-html method to TextNode  and its unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -34,7 +34,45 @@ class TestTextNode(unittest.TestCase):
         node1 = TextNode("Text", TextType.TEXT)  # Default url is None
         node2 = TextNode("Text", TextType.TEXT, None)  # Explicitly None
         self.assertEqual(node1, node2)
-
+    
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = TextNode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_bold(self):
+        node = TextNode("Bold text example", TextType.BOLD)
+        html_node = TextNode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "Bold text example")
+        self.assertEqual(html_node.props, None)
+    
+    def test_italic(self):
+        node = TextNode("Italic text example", TextType.ITALIC)
+        html_node = TextNode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "Italic text example")
+        self.assertEqual(html_node.props, None)
+    
+    def test_link(self):
+        node = TextNode("Click here", TextType.LINK, url="http://example.com")
+        html_node = TextNode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click here")
+        self.assertEqual(html_node.props, {"href": "http://example.com"})
+    
+    def test_image(self):
+        node = TextNode("Alt text example", TextType.IMAGE, url="http://example.com/image.png")
+        html_node = TextNode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "http://example.com/image.png", "alt": "Alt text example"})
+    
+    def test_invalid_text_type(self):
+        node = TextNode("Invalid type example", None)
+        with self.assertRaises(Exception):
+            TextNode.text_node_to_html_node(node)
 
 if __name__ == "__main__":
     unittest.main()
